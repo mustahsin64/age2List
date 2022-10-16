53b3dea0372e97b51694283e00132582e009943b
siam <mustahsin64@gmail.com>
2022-10-16 15:00:47 +0600

    - viewset, and router is omitted for now, using function based api-view - getUnitList by filtering with hp

--- age2List/urls.py
+++ age2List/urls.py
@@ -1,29 +1,33 @@
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth.models import User
-from agestats.views import UnitViewSet
-from rest_framework import routers, serializers, viewsets
+from agestats.views import UnitViewSet,unitList,unitAdd,unitFilterWithHp
+# from rest_framework import routers, serializers, viewsets
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+# # Serializers define the API representation.
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url', 'username', 'email', 'is_staff']
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# # ViewSets define the view behavior.
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'units',UnitViewSet)
+# # Routers provide an easy way of automatically determining the URL conf.
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
+# router.register(r'units',UnitViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('unit-list/', unitList, name="unit-list"),
+    path('unitFilterWithHp/<str:hp>/', unitFilterWithHp, name="unitFilterWithHp"),
 ]
+
--- agestats/views.py
+++ agestats/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from agestats.models import Unit
 from agestats.serializers import UnitSerializer
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 from rest_framework import viewsets
 
@@ -11,3 +14,26 @@ class UnitViewSet(viewsets.ModelViewSet):
     """
     queryset = Unit.objects.all().order_by('name')
     serializer_class = UnitSerializer
+
+
+@api_view(['GET'])
+def unitList(request):
+	queryset = Unit.objects.all().order_by('id')
+	serializer = UnitSerializer(queryset, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def unitFilterWithHp(request,hp):
+	queryset = Unit.objects.filter(hp=hp)
+	serializer = UnitSerializer(queryset,many=True)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def unitAdd(request):
+	serializer = UnitSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
